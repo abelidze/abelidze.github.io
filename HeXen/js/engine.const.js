1be18c05eba31c68232a308069b2b08bc8ad6d9a
Vladimir Glushkov <glushkov.vk@students.dvfu.ru>
2017-07-11 14:17:03 +1000

    Update game objects

--- HeXen/js/engine.const.js
+++ HeXen/js/engine.const.js
@@ -1,37 +1,44 @@
+const ObjectTypes = {
+    NONE: 0,
+    STATIC: 1,
+    DYNAMIC: 2,
+    PLAYER: 3,
+    ENEMY: 4
+};
 const CellState = {
-	INVISIBLE: 0,
-	EMPTY: 1,
-	OBJECT: 2
+    INVISIBLE: 0,
+    EMPTY: 1,
+    OBJECT: 2
 };
 
 const InteractResult = {
-	NOTHING: 0,
-	MOVED: 1,
-	ATTACK: 2,
-	DIE: 3
+    NOTHING: 0,
+    MOVED: 1,
+    ATTACK: 2,
+    DIE: 3
 };
 
 const GameState = {
-	MENU: 0,
-	PAUSE: 1,
-	TURN: 2,
-	LOADING: 3,
-	RESULTS: 4
+    MENU: 0,
+    PAUSE: 1,
+    TURN: 2,
+    LOADING: 3,
+    RESULTS: 4
 };
 
 const DoorState = {
-	CLOSED: 0,
-	OPENED: 1
+    CLOSED: 0,
+    OPENED: 1
 };
 const GameResult = {
-	NONE: 0,
-	WIN: 1,
-	LOSE: 2
+    NONE: 0,
+    WIN: 1,
+    LOSE: 2
 };
 
 const HexDirections = [
-	[+1, 0], [+1, -1], [0, -1],
-	[-1, 0], [-1, +1], [0, +1]
+    [+1, 0], [+1, -1], [0, -1],
+    [-1, 0], [-1, +1], [0, +1]
 ];
 
 const ClickRadius = 16;
--- HeXen/js/engine.objects.js
+++ HeXen/js/engine.objects.js
@@ -5,10 +5,15 @@
 function GameObject(cell) {
 	this.cell = cell;
     this.rotation = 0;
+    this._type_ = ObjectTypes.NONE;
 }
 
+GameObject.prototype.GetType = function(object, callback) {
+    //do smth
+};
+
 GameObject.prototype.Collide = function(object, callback) {
-	// Do Stuff
+  //do smth
 };
 
 GameObject.prototype.Destroy = function() {
@@ -19,30 +24,39 @@ GameObject.prototype.Destroy = function() {
 /* STATIC */
 function StaticObject(cell) {
     GameObject.call(this, cell);
+    this._type_ = ObjectTypes.STATIC;
 }
 StaticObject.prototype = Object.create(GameObject.prototype);
 
 function Trigger(cell) {
     StaticObject.call(this, cell);
+    this.action = null;
 
 }
 Trigger.prototype = Object.create(StaticObject.prototype);
 
-function Obstacle(cell) {
+function Obstacle(cell, sprite) {
 	StaticObject.call(this, cell);
+	this.sprite = sprite;
 }
 Obstacle.prototype = Object.create(StaticObject.prototype);
 
-function Wall(cell) {
-    Obstacle.call(this, cell);
+function Wall(cell, sprite) {
+    Obstacle.call(this, cell, sprite);
 }
 Wall.prototype = Object.create(Obstacle.prototype);
 
-function Door(cell) {
-    Obstacle.call(this, cell);
+function Door(cell, sprite) {
+    Obstacle.call(this, cell, sprite);
     this.status = DoorState.CLOSED;
 }
 Door.prototype = Object.create(Obstacle.prototype);
+
+function Container(cell, sprite, content) {
+    Obstacle.call(this, cell, sprite, sprite);
+    this.content = content;
+}
+Container.prototype = Object.create(Obstacle.prototype);
 
 /* DYNAMIC */
 function DynamicObject(cell) {
@@ -64,20 +78,17 @@ function Actor(sprite, cell) {
     DynamicObject.call(this, cell);
 	this.sprite = sprite;
 	this.actionPoints = 0;
+	this.inventory = [];
 	// this.cell = cell;
 }
 Actor.prototype = Object.create(DynamicObject.prototype);
 
 function Player(sprite, cell) {
     Actor.call(this, sprite, cell);
-	// this.sprite = sprite;
-	// this.cell = cell;
 }
 Player.prototype = Object.create(Actor.prototype);
 
 function Enemy(sprite, cell) {
     Actor.call(this, sprite, cell);
-	// this.sprite = sprite;
-	// this.cell = cell;
 }
 Enemy.prototype = Object.create(Actor.prototype);
