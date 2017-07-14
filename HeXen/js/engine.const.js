ca9ecca366c5c6852f910a0a284258c7fd3748c0
Vladimir Glushkov <glushkov.vk@students.dvfu.ru>
2017-07-14 13:46:09 +1000

    Moved some enums fron config to consts

--- HeXen/js/engine.const.js
+++ HeXen/js/engine.const.js
@@ -50,3 +50,26 @@ const EventType = {
 const ClickRadius = 16;
 
 const EPS = Number.EPSILON;
+
+
+const SoundNames = [
+	"./sounds/HIT.mp3",
+	"./sounds/GENERATOR.mp3",
+	"./sounds/ROAR.mp3",
+	"./sounds/RESTLESS_1.mp3",
+	"./sounds/RESTLESS_2.mp3",
+	"./sounds/ALARM.mp3",
+	"./sounds/ALARM.mp3",
+	"./sounds/HORIZON_SHADOWLANDS_1.mp3",
+	"./sounds/BRIDGE_SHADOWLANDS_2.mp3"
+];
+
+const SoundIndex = {
+	HIT: 0,
+	GENERATOR: 1,
+	ROAR: 2,
+	RESTLESS_1: 3,
+	RESTLESS_2: 4,
+	ALARM: 5,
+	HORIZON: 6
+};
