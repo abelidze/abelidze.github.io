630acb9a61bda6cad79fe4d1173ae9cb9c8d8229
SkillMasters <avataraang399@gmail.com>
2018-01-01 22:58:42 +1000

    Improved usability, added videos

--- js/skillmasters.js
+++ js/skillmasters.js
@@ -8,11 +8,23 @@ $( document ).ready(function() {
     loadContent(getLocalPath($(".current")));
 
     $("#menu li a").click(navigationClick);
+
+    $(".video_preview img").click(loadVideo);
+
+    $("body").append (
+        $("<div>")
+        .attr("id", "overlay")
+        .on("overlay:toggle", toggleOverlay)
+        .click( function () {
+            $(this).trigger("overlay:toggle");
+        })
+    );
 });
 
 function loadContent(content) {
-    $("#content").fadeOut(200, function () {
-        $(this).fadeIn(400).load(content, function () {
+    $content = $("#content");
+    $content.children().fadeOut(400, function () {
+        $content.load(content, function () {
             $("img").on("error", function() {
                 $(this).attr("src", "images/no-img.png");
             });
@@ -30,8 +42,33 @@ function refreshContent($current) {
     loadContent(getLocalPath($current));
 }
 
+function loadVideo() {
+    $this = $(this);
+    if ($this.attr("href") == undefined) return;
+
+    $("#overlay")
+    .trigger("overlay:toggle")
+    .append (
+        $("<div>").append (
+            $("<iframe>").attr({
+                width: "100%",
+                height: "100%",
+                src: $this.attr("href"),
+                frameborder: 0,
+                gesture: "media",
+                allow: "encrypted-media",
+                allowfullscren: 1
+            })
+        )
+    );
+}
+
 function getLocalPath($current) {
     return contentPath + $current.parent().attr("class") + '.html';
+}
+
+function toggleOverlay() {
+    $(this).text("").fadeToggle(200);
 }
 
 function navigationClick() {
